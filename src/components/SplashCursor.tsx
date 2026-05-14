4de8f3393de172fbd155874d44324d9e33d34ebd
aswathas20 <aswathas20@gmail.com>
2026-05-14 19:57:27 +0000

    Add missing SplashCursor component for production build

---
+++ src/components/SplashCursor.tsx
@@ -0,0 +1,18 @@
+type SplashCursorProps = {
+  DENSITY_DISSIPATION?: number;
+  VELOCITY_DISSIPATION?: number;
+  PRESSURE?: number;
+  CURL?: number;
+  SPLAT_RADIUS?: number;
+  SPLAT_FORCE?: number;
+  COLOR_UPDATE_SPEED?: number;
+  SHADING?: boolean;
+  RAINBOW_MODE?: boolean;
+  COLOR?: string;
+  COLORS?: string[];
+};
+
+export default function SplashCursor(_props: SplashCursorProps) {
+  return null;
+}
+
